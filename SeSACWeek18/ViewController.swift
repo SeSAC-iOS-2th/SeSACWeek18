3f08de7da26c7d499bea9dcd88fb1a9b3556e3da
joongwon <wnddnjs823@ajou.ac.kr>
2022-11-02 17:27:38 +0900

    회원 가입, 로그인, 프로필 기능 Class

--- SeSACWeek18/ViewController.swift
+++ SeSACWeek18/ViewController.swift
@@ -6,61 +6,18 @@
 //
 
 import UIKit
-import RxSwift
-import RxAlamofire
-import RxDataSources
 
 class ViewController: UIViewController {
     
-    let disposeBag = DisposeBag()
-    
-    //lazy var?
-    lazy var dataSource = RxTableViewSectionedReloadDataSource<SectionModel<String, Int>>(configureCell: { datasource, tableView, indexPath, item in
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
-        cell.textLabel?.text = "\(item)"
-        return cell
-    })
-    
-    @IBOutlet weak var tableView: UITableView!
+    let api = APIService()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        testRxAlamofire()
-        testRxDataSource()
+        api.profile()
         
     }
     
-    func testRxDataSource() {
-        
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
-        
-        dataSource.titleForHeaderInSection = { dataSource, index in
-            return dataSource.sectionModels[index].model
-        }
-        
-        Observable.just([
-            SectionModel(model: "첫번째 섹션", items: [1, 2, 3]),
-            SectionModel(model: "두번째 섹션", items: [1, 2, 3]),
-            SectionModel(model: "세번째 섹션", items: [1, 2, 3])
-        ])
-        .bind(to: tableView.rx.items(dataSource: dataSource))
-        .disposed(by: disposeBag)
-        
-    }
-    
-    func testRxAlamofire() {
-        //Success Or Error => <Single>
-        let url = "https://api.unsplash.com/search/photos?query=apple";
-        request(.get, url, headers: ["Authorization": "Client-ID 4RB3xKIv1jUJ2iuqNmbICVTbJ2xVp0WSPPh4zmmVvvY"])
-            .data()
-            .subscribe { value in
-                print(value)
-            }
-            .disposed(by: disposeBag)
-    }
-
 
 }
 
